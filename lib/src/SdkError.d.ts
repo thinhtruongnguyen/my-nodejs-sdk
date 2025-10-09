350ad74741cc406a406ff71623f89e7fc6207a79
Thinh Nguyen <thinh.truong.nguyen@aioz.io>
2025-10-09 10:45:35 +0700

    rm nodemodule and lib

--- lib/src/SdkError.d.ts
+++
@@ -1,8 +0,0 @@
-import ProblemDetails from "./custom-models/ProblemDetails";
-export default class SdkError extends Error {
-    readonly statusCode: number;
-    readonly status?: string;
-    readonly problemDetails?: ProblemDetails;
-    constructor(statusCode: number, problemDetails?: ProblemDetails);
-    getProblemDetails(): ProblemDetails | undefined;
-}
--- lib/src/custom-models/ProblemDetails.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface ProblemDetails {
-    status: string;
-    message: string;
-    type: string;
-    title: string;
-    detail: string;
-}
